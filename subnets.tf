9d10797e059c225dc8869c0b451c51f2913913ee
suneel-sagi <52102251+suneel-sagi@users.noreply.github.com>
2020-12-24 15:40:09 +0530

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.My_vpc.id
+  vpc_id                  = "${aws_vpc.My_vpc.id}"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   tags = {
@@ -7,11 +7,10 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.My_vpc.id
+  vpc_id                  = "${aws_vpc.My_vpc.id}"
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "false"
   availability_zone        = "ap-southeast-1b"
-
   tags = {
     Name = "private_subnet"
   }
